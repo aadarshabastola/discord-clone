ecfeaeddedc6fd2f3467da613e7d0f59dece3cc0
Aadarsha Bastola <aadarsha.com@gmail.com>
2023-12-31 10:20:38 -0700

    Initial Commit

---
+++ components/file-upload.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { X } from "lucide-react";
+import Image from "next/image";
+
+import { UploadDropzone } from "@/lib/uploadthing";
+
+import "@uploadthing/react/styles.css";
+import { error } from "console";
+
+interface FileUploadProps {
+    onChange: (url?: String) => void;
+    value: string;
+    endpoint: "messageFile" | "serverImage"
+}
+
+export const FileUpload = ({
+    onChange,
+    value,
+    endpoint
+}: FileUploadProps) => {
+
+    const fileType = value?.split(".").pop();
+
+    if (value) {
+        return (
+           <div className=" relative h-20 w-20">
+            <Image fill src={value} alt="Upload" className=" roundef"></Image>
+            <button  
+            onClick={() => onChange("")}
+            className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+            type="button"
+            >
+                <X className=" h-4 w-4" />
+               
+           </button>
+           </div> 
+        )
+    }
+
+    return (
+        <UploadDropzone
+            endpoint={endpoint}
+            onClientUploadComplete={(res) => {
+                onChange(res?.[0].url);
+            }}
+            onUploadError={(error: Error) => {
+                console.log(error);
+            }}
+        />
+    )
+}
