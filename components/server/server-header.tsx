4faf8b98bfef5ffd3eab89002d140720495bf4ae
Aadarsha Bastola <aadarsha.com@gmail.com>
2024-01-02 22:06:17 -0700

    added leave and delete server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -69,6 +69,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                    onClick={() => onOpen("deleteServer", { server })}
                         className=" text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto" />
@@ -76,6 +77,7 @@ export const ServerHeader = ({
                 )}
                 {!isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("leaveServer", { server })}
                         className=" px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto" />
