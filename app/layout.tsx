28a2acb7e6e19f32cb3b97780a3144d42f9e4ece
Aadarsha Bastola <aadarsha.com@gmail.com>
2024-01-03 06:38:09 -0700

    initialized socket and created chat input

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { cn } from '@/lib/utils'
 import { ModalProvider } from '@/components/providers/modal-provider'
+import { SocketProvider } from '@/components/providers/socket-provider'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -28,8 +29,10 @@ export default function RootLayout({
             enableSystem
             storageKey='discord-theme'
           >
+            <SocketProvider>
             <ModalProvider/>
             {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
